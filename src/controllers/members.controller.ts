f00ae992b082d8a4316df5c29a243b3c83bed772
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2020-01-17 15:18:05 +0100

    Fixed 500 error

--- src/controllers/members.controller.ts
+++ src/controllers/members.controller.ts
@@ -42,7 +42,7 @@ export class MembersController extends BaseRoute {
             let member: MembersEntity = new MembersEntity();
             const {email,firstName,lastName,telephone,role,imageUrl,twitterUrl,linkedinUrl,githubUrl} = request.body;
             const check = await this.memberRepo.getOneMember(email);
-            if(check.email) {
+            if(check !== undefined) {
                 response.status(400).json({
                     message: "This email address already exist",
                     error: true
@@ -64,7 +64,7 @@ export class MembersController extends BaseRoute {
                 const payload = await this.memberRepo.saveMember(member);
 
                 this.service.Email(member.email, 'New Member Registration', 
-                    this.service.html('<p style="color: #000">Hello ' + member.firstName + ' ' + member.lastName + ', Thank you for registering as a member in our community. <br/><br/>We will get back to you shortly.</p>'));
+                   this.service.html('<p style="color: #000">Hello ' + member.firstName + ' ' + member.lastName + ', Thank you for registering as a member in our community. <br/><br/>We will get back to you shortly.</p>'));
 
                 response.json({
                     message: "Member created successfully",
@@ -73,7 +73,9 @@ export class MembersController extends BaseRoute {
                 });
             }
         } catch (err) {
+            console.log(err)
             response.status(500).send(err);
+
         }
     }
 };
