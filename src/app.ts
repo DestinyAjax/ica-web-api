1fa03306bb2dc96a9db5323c6d8ef9962c43128c
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2020-04-10 10:23:10 +0100

    Add express url encoded

--- src/app.ts
+++ src/app.ts
@@ -22,8 +22,10 @@ class App {
     }
 
     private initializeMiddlewares() {
-        this.app.use(bodyParser.urlencoded({extended: false}));
-        this.app.use(bodyParser.json());
+        this.app.use(express.json({limit: '50mb'}));
+        this.app.use(express.urlencoded({limit: '50mb'}));
+        // this.app.use(bodyParser.urlencoded({extended: false}));
+        // this.app.use(bodyParser.json());
         this.app.use(cors());
     }
 
